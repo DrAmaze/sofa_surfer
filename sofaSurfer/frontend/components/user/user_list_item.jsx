3a4e6ba89a001e906cc4d8be49cbf10900f94fd9
jacob <jbutler8@terpmail.umd.edu>
2017-11-29 10:17:37 -0800

    style host list on spot show page

--- sofaSurfer/frontend/components/user/user_list_item.jsx
+++ sofaSurfer/frontend/components/user/user_list_item.jsx
@@ -8,13 +8,25 @@ class UserListItem extends React.Component {
       email,
       phone,
       age,
-      about_me
+      about_me,
+      img_url
     } = user;
+
+    let image;
+    if (img_url) {
+      image = <img src={img_url} alt='user image' />
+    } else {
+      image = <img src='https://staticcdn.selio.com/adoos-static/img/user_default.png' alt='blank image' />
+    }
 
     return (
       <li className='spot-host'>
-        <h3>{ username }</h3>
+
+        <div className='user-image'>
+          {image}
+        </div>
         <div className='host-info'>
+          <h3>{ username }</h3>
           <div> { email } </div>
           <div> { phone } </div>
           <div> { age } </div>
