0e8304ded15e0e212244bc5cd5001bfc241f6356
jacob <jbutler8@terpmail.umd.edu>
2018-04-05 18:39:13 -0700

    complete unit testing for user model

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,59 +1,92 @@
 require 'rails_helper'
+require 'factories'
 
 RSpec.describe User, type: :model do
   describe 'password encryption' do
 
-    # it 'does not save passwords to the database' do
-    #
-    #   spot1 = Location.find_by_neighborhood('presidio')
-    #
-    #   User.create!({
-    #       username: 'anastassia',
-    #       password: 'nunyabusiness',
-    #       location: Location.find_by_neighborhood('presidio'),
-    #       email: 'ana@taser.com'
-    #   })
-    #   user = User.find_by_username('anastassia')
-    #   expect(user.password).not_to be('nunyabusiness')
-    # end
+    it 'does not save passwords to the database' do
 
-    # Add more tests here!
-
+      User.create!({
+          username: 'anastassia',
+          password: 'nunyabusiness',
+          location: Location.find(1),
+          email: 'ana@taser.com'
+      })
+      user = User.find_by_username('anastassia')
+      expect(user.password).not_to be('nunyabusiness')
+    end
   end
-end
 
-# RSpec.describe User, :type => :model do
-#
-#   before(:all) do
-#     @user1 = create(:user)
-#   end
-#
-#   it "is valid with valid attributes" do
-#     expect(@user1).to be_valid
-#   end
-#
-#   it "has a unique username" do
-#     user2 = build(:user, email: "test@ing.com")
-#     expect(user2).to_not be_valid
-#   end
-#
-#   it "has a unique email" do
-#     user2 = build(:user, username: "Test")
-#     expect(user2).to_not be_valid
-#   end
-#
-#   it "is not valid without a password" do
-#     user2 = build(:user, password: nil)
-#     expect(user2).to_not be_valid
-#   end
-#
-#   it "is not valid without a username" do
-#     user2 = build(:user, username: nil)
-#     expect(user2).to_not be_valid
-#   end
-#
-#   it "is not valid without an email" do
-#     user2 = build(:user, email: nil)
-#     expect(user2).to_not be_valid
-#   end
-# end
+  describe 'Create a user' do
+
+    it "is valid with minimal attributes" do
+      User.create!({
+          username: 'AlexHonnold',
+          password: 'password',
+          location: Location.find(1),
+          email: 'alex@honnold.climbing'
+      })
+      user = User.find_by_username('Alex Honnold')
+      expect(user).to be_valid
+    end
+
+    it "is valid with all attributes" do
+      User.create!({
+          username: 'AlexHonnold',
+          password: 'password',
+          location: Location.find(1),
+          email: 'alex@honnold.climbing',
+          phone: '240-673-5634',
+          age: 31,
+          hosting: false,
+          about_me: 'I love to rock climb',
+          street: 'Yosemite Ave',
+          img_url: 'https://www.nationalgeographic.com/content/dam/adventure/photos/2017/stories/alex-honnold/alex-honnold-summit/alex-honnold-free-solo-summit-el-capitan.ngsversion.1496520016051.adapt.1900.1.jpg'
+      })
+      user = User.find_by_username('Alex Honnold')
+      expect(user).to be_valid
+    end
+
+    it "does not create a user with a taken username" do
+      user1 = User.create({
+          username: 'Sample User',
+          password: 'password',
+          location: Location.find(1),
+          email: 'fake@one.com'
+      })
+      user2 = User.find_by_username('Sample User')
+      expect(user2.email).to_not equal(user1)
+    end
+
+    it "is not valid without a password" do
+      user1 = User.create({
+          username: 'Paul Bunyun',
+          password: '',
+          location: Location.find(1),
+          email: 'paul@gmail.com'
+      })
+      expect(user1).to_not be_valid
+    end
+
+    it "is not valid without a username" do
+      user1 = User.create({
+          username: '',
+          password: 'password',
+          location: Location.find(1),
+          email: 'paul@gmail.com'
+      })
+      expect(user1).to_not be_valid
+    end
+
+    it "is not valid without an email" do
+      user1 = User.create({
+          username: 'Benedict Stevenson III',
+          password: 'password',
+          location: Location.find(1),
+          email: ''
+      })
+      expect(user1).to_not be_valid
+    end
+  end
+
+end
