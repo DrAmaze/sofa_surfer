e7788b1c9511c97fd96a9b125d24300663d31b4c
jacob <jbutler8@terpmail.umd.edu>
2018-04-11 07:50:29 -0700

    complete creation specs for booking model

--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -34,19 +34,39 @@ RSpec.describe Booking, type: :model do
     end
 
     it 'is invalid without arrival date' do
-
+      booking = Booking.create({
+          traveler_id: 5,
+          host_id: 1,
+          location_id: 1,
+          departure: DateTime.new(2018,5,12,4,5,6),
+          description: 'This will be a test adventure',
+          host: User.find(3)
+        })
+        expect(booking).to_not be_valid
     end
 
     it 'is invalid without departure date' do
-
-    end
-
-    it 'is invalid without host ID' do
-
+      booking = Booking.create({
+          traveler_id: 5,
+          host_id: 1,
+          location_id: 1,
+          arrival: DateTime.new(2018,5,3,4,5,6),
+          description: 'This will be a test adventure',
+          host: User.find(3)
+        })
+        expect(booking).to_not be_valid
     end
 
     it 'is invalid without location ID' do
-
+      booking = Booking.create({
+          traveler_id: 5,
+          host_id: 1,
+          arrival: DateTime.new(2018,5,3,4,5,6),
+          departure: DateTime.new(2018,5,12,4,5,6),
+          description: 'This will be a test adventure',
+          host: User.find(3)
+        })
+        expect(booking).to_not be_valid
     end
   end
 
