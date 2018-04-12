54ae96e51d56a387b31c3d2af7645cd448500efa
jacob <jbutler8@terpmail.umd.edu>
2018-04-12 10:31:07 -0700

    write entry file test

---
+++ frontend/__tests__/session-tests.js
@@ -0,0 +1,16 @@
+// import React from 'react';
+// import { shallow, mount, render } from 'enzyme';
+// import Login from './components/session_form/login';
+// import Signup from './components/session_form/signup';
+//
+// const Enzyme = require('enzyme');
+// const EnzymeAdapter = require('enzyme-adapter-react-16');
+//
+// Enzyme.configure({ adapter: new EnzymeAdapter() });
+//
+// describe('Login Component', () => {
+//
+//   it('Should render without errors', () => {
+//
+//   });
+// });
