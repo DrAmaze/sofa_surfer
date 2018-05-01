a415a494ec12bf33cfcce3fd56b5d9bf04b99d24
jacob <jbutler8@terpmail.umd.edu>
2018-04-30 18:05:16 -0700

    remove withRouter

--- frontend/components/user/user_hosting_form.jsx
+++ frontend/components/user/user_hosting_form.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class UserHostingForm extends React.Component {
   constructor(props) {
@@ -38,4 +38,4 @@ class UserHostingForm extends React.Component {
     );
   }
 }
-export default withRouter(UserHostingForm);
+export default UserHostingForm;
