89ec8dc07e5644ecebb4cf0112a2ecf876e6429b
jacob <jbutler8@terpmail.umd.edu>
2018-05-14 10:55:00 -0700

    update location index test

--- frontend/__tests__/location_index-tests.js
+++ frontend/__tests__/location_index-tests.js
@@ -10,11 +10,24 @@ const EnzymeAdapter = require('enzyme-adapter-react-16');
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 describe('<LocationContainer>', () => {
-  it('renders 10 locations', () => {
+  it('renders an unordered list of locations', () => {
     const wrapper = shallow(<Location fetchSpots={ () => {} } />);
-    expect(wrapper.find(<SpotIndexItem />).length).toEqual(10);
+
+    expect(wrapper.find('ul').length).toEqual(1);
   });
 
+  it('renders an unordered list of locations', () => {
+    // const wrapper = mount(<Location fetchSpots={ () => {} } />);
+    //
+    // expect(wrapper.find('ul')).toContain('li');
+  });
+
+  describe('<LocationListItem>', () => {
+    it('renders the item', () => {
+      // const wrapper = shallow(<SpotIndexItem key={0} id={0} />);
+      // expect(wrapper.find('li')).toHaveLength(1);
+    });
+  });
 
 
 });
