f99c40a191458725133f9ce4a5667cc8ada2d6c2
jacob <jbutler8@terpmail.umd.edu>
2018-05-31 09:03:44 -0700

    abstract modal style

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -122,28 +122,7 @@ class Profile extends React.Component {
       phoneNumber = '';
     }
 
-    const style = {
-      overlay : {
-        position        : 'fixed',
-        top             : 0,
-        left            : 0,
-        right           : 0,
-        bottom          : 0,
-        backgroundColor : 'rgba(25, 25, 25, 0.90)',
-        zIndex          : 10
-      },
-      content : {
-        position        : 'relative',
-        top             : '10px',
-        border          : '1px solid #ccc',
-        zIndex          : 11,
-        background      : 'white',
-        borderRadius    : '5px',
-        width           : '70%',
-        marginLeft      : 'auto',
-        marginRight     : 'auto',
-      }
-    };
+    const style = modalUserUpdateStyle;
 
     const userUpdate = <UserUpdateFormContainer
       closeModal={ this.closeModal }
