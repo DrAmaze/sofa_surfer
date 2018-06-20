cd31da4bb344558a168213dae8645c22484b68a1
jacob <jbutler8@terpmail.umd.edu>
2018-06-20 13:21:12 -0700

    format booking specs

--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -7,66 +7,66 @@ RSpec.describe Booking, type: :model do
       dep = DateTime.new(2018,5,12,4,5,6)
 
       Booking.create!({
-          traveler_id: 5,
-          host_id: 1,
-          location_id: 1,
-          arrival: arr,
-          departure: dep,
-          host: User.find(3)
-        })
+        traveler_id: 5,
+        host_id: 1,
+        location_id: 1,
+        arrival: arr,
+        departure: dep,
+        host: User.find(3)
+      })
 
-        booking = Booking.find_by_departure(dep)
-        expect(booking).to be_valid
+      booking = Booking.find_by_departure(dep)
+      expect(booking).to be_valid
     end
 
     it 'is valid with all attributes' do
       Booking.create!({
-          traveler_id: 5,
-          host_id: 1,
-          location_id: 1,
-          arrival: DateTime.new(2018,5,3,4,5,6),
-          departure: DateTime.new(2018,5,12,4,5,6),
-          description: 'This will be quite an adventure',
-          host: User.find(3)
-        })
-        booking = Booking.find_by_description('This will be quite an adventure')
-        expect(booking).to be_valid
+        traveler_id: 5,
+        host_id: 1,
+        location_id: 1,
+        arrival: DateTime.new(2018,5,3,4,5,6),
+        departure: DateTime.new(2018,5,12,4,5,6),
+        description: 'This will be quite an adventure',
+        host: User.find(3)
+      })
+      booking = Booking.find_by_description('This will be quite an adventure')
+      expect(booking).to be_valid
     end
 
     it 'is invalid without arrival date' do
       booking = Booking.create({
-          traveler_id: 5,
-          host_id: 1,
-          location_id: 1,
-          departure: DateTime.new(2018,5,12,4,5,6),
-          description: 'This will be a test adventure',
-          host: User.find(3)
-        })
-        expect(booking).to_not be_valid
+        traveler_id: 5,
+        host_id: 1,
+        location_id: 1,
+        departure: DateTime.new(2018,5,12,4,5,6),
+        description: 'This will be a test adventure',
+        host: User.find(3)
+      })
+      expect(booking).to_not be_valid
     end
 
     it 'is invalid without departure date' do
       booking = Booking.create({
-          traveler_id: 5,
-          host_id: 1,
-          location_id: 1,
-          arrival: DateTime.new(2018,5,3,4,5,6),
-          description: 'This will be a test adventure',
-          host: User.find(3)
-        })
-        expect(booking).to_not be_valid
+        traveler_id: 5,
+        host_id: 1,
+        location_id: 1,
+        arrival: DateTime.new(2018,5,3,4,5,6),
+        description: 'This will be a test adventure',
+        host: User.find(3)
+      })
+      expect(booking).to_not be_valid
     end
 
     it 'is invalid without location ID' do
       booking = Booking.create({
-          traveler_id: 5,
-          host_id: 1,
-          arrival: DateTime.new(2018,5,3,4,5,6),
-          departure: DateTime.new(2018,5,12,4,5,6),
-          description: 'This will be a test adventure',
-          host: User.find(3)
-        })
-        expect(booking).to_not be_valid
+        traveler_id: 5,
+        host_id: 1,
+        arrival: DateTime.new(2018,5,3,4,5,6),
+        departure: DateTime.new(2018,5,12,4,5,6),
+        description: 'This will be a test adventure',
+        host: User.find(3)
+      })
+      expect(booking).to_not be_valid
     end
   end
 
--- spec/review_spec.rb
+++ spec/review_spec.rb
@@ -3,6 +3,18 @@ require 'rails_helper'
 RSpec.describe Review, type: :model do
   describe 'model' do
     it 'is valid with minimal attributes' do
+     #  body = 'My time with cpellew4 was beautiful. Very hospitible'
+     #  Review.create!({
+     #    user_id: 5,
+     #    author_id: 1,
+     #    recommendation: true,
+     #    rating: 4,
+     #    body: body,
+     #    user: User.find_by_email('rmorrowf@foxnews.com')
+     #  })
+     #
+     # review = Review.find_by_body(body)
+     # expect(review).to be_valid
     end
 
     it 'is valid with all attributes' do
